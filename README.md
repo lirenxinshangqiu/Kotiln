c703d2b93c5792deca1612f2d2287597ec4e60ee
lushan <shan.lu@mtime.com>
2016-12-15 14:53:16 +0800

    KotLin test文件

--- README.md
+++
@@ -1,2 +0,0 @@
-# Kotiln
-Kotlin demo
---
+++ src/Lamdba.java
@@ -0,0 +1,8 @@
+/**
+ * Created by lsmtime on 2016/12/15.
+ */
+public class Lamdba {
+    public static void main(String args[]) {
+        System.out.print("123");
+    }
+}
