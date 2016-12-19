7ffa159aa49477f7504dd7a999f515a62ed7dd0a
lushan <shan.lu@mtime.com>
2016-12-19 11:19:24 +0800

    构造函数

--- src/com/lsmtime/demo/Base.kt
+++ src/com/lsmtime/demo/Base.kt
@@ -3,8 +3,10 @@ package com.lsmtime.demo
 /**
  * Created by lsmtime on 2016/12/19.
  */
-
-open class Base {
+/**
+ * open注解和java中的final相反,允许别的类进行继承,在默认情况下Kotlin中的类都不能够被其他的类继承,默认为final类
+ */
+ open class Base {
 
     private var age: Int
 
@@ -12,7 +14,7 @@ open class Base {
         this.age = age
     }
 
-    fun getAge(): Int? {
+    open  fun getAge(): Int? {
         return age
     }
 }
--- src/com/lsmtime/demo/Invoice.kt
+++ src/com/lsmtime/demo/Invoice.kt
@@ -3,10 +3,23 @@ package com.lsmtime.demo
 /**
  * Created by lsmtime on 2016/12/16.
  */
-class Invoice(name: String?) : Base(10) {
-    var name: String? = name
+class Invoice : Base {
+    var name: String?
+
+    /**
+     * 如果没有构造函数,需要在构造函数后面添加super字段条用父类构造
+     */
+    constructor(age: Int, name: String?) : super(age) {
+        this.name = name
+    }
+
     fun getBaseInfo(): String? {
-        return name +" "+getAge()
+        return name
+    }
+
+    //如果一个方法不想被继承,在前面加关键字final
+    final override fun getAge(): Int? {
+        return 1
     }
 
 
